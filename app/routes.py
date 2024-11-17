397b93dd5017d3710ff17bad757288c3962f6e09
Ankit-dev-cod <andrea.molliconi@gmail.com>
2024-11-17 13:57:01 +0100

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template
+import os
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return "Welcome to Mindmap AI! Your app is running successfully."
+    return render_template("index.html")
 
 @main.route('/upload', methods=['POST'])
 def upload_pdf():
     file = request.files['file']
     if not file:
         return jsonify({"error": "No file uploaded"}), 400
-    return jsonify({"message": "File uploaded successfully"})
+    file_path = os.path.join("uploads", file.filename)
+    file.save(file_path)
+    return jsonify({"message": "File uploaded successfully", "file": file.filename})
