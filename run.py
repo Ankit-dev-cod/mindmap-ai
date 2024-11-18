18424e6cab4e924d40b81cf637c9dc598e75d38e
Ankit-dev-cod <andrea.molliconi@gmail.com>
2024-11-18 13:07:56 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default a 5000 se la variabile PORT non è definita
+    app.run(host='0.0.0.0', port=port)
